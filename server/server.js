d108287546a1f36f34a34e0601aef99c70327124
Elshaabany <elshaabany@protonmail.com>
2023-06-11 08:38:48 +0300

    Add read the data function

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import * as fs from 'fs';
+
+const data = (() => {
+	try {
+		return fs.readFileSync('TestData.json', 'utf8');
+	} catch (err) {
+		console.error('fatal error: data access denied');
+		throw err;
+	}
+})();
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-	console.log('app is running at http://localhost:3000');
+app.listen(5000, () => {
+	console.log('app is running at http://localhost:5000 ');
 });
