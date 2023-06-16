5d308cbeb3dc59046d6db5b0270160152283f980
Elshaabany <elshaabany@protonmail.com>
2023-06-16 21:22:54 +0300

    Update: no need for useEffect in Rank

--- client/src/Rank.js
+++ client/src/Rank.js
@@ -1,24 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Rank({ score, restartQuiz, wordsLength }) {
   const [rank, setRank] = useState("");
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch("http://localhost:5000/rank", {
-          method: "POST",
-          body: JSON.stringify({ score: (+score / wordsLength) * 100 }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-        setRank(data.rank);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, [score, wordsLength]);
+  (async () => {
+    try {
+      const response = await fetch("http://localhost:5000/rank", {
+        method: "POST",
+        body: JSON.stringify({ score: (+score / wordsLength) * 100 }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setRank(data.rank);
+    } catch (error) {
+      console.log(error);
+    }
+  })();
 
   if (!rank) return <h2>loading...</h2>;
 
