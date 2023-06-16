6ccdfe8d41240fc86575e86473fd4d15901b41e6
Elshaabany <elshaabany@protonmail.com>
2023-06-16 17:17:11 +0300

    Fetch words form server

--- client/src/Practice.js
+++ client/src/Practice.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 const choices = ["noun", "adverb", "adjective", "verb"];
 
 const Practice = () => {
+  const [words, setWords] = useState([]);
+  const [wordIndex, setWordIndex] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("http://localhost:5000/words");
+        const data = await response.json();
+        setWords(data.words);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  const currentWord = words[wordIndex];
+
+  if (!currentWord) return <h2>loading...</h2>;
+
   return (
     <>
-      <h2>word</h2>
+      <h2>{currentWord.word}</h2>
       {choices.map((choice) => (
         <button>{choice}</button>
       ))}
