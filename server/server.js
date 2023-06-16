2ffb0c1a903f3bffd3a9654f27d91cdb53fcc6c8
Elshaabany <elshaabany@protonmail.com>
2023-06-17 00:42:04 +0300

    Add error handling

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { readFileSync } from 'fs';
+import cors from 'cors';
 import randWords from './randomWords.js';
 
 const { wordList, scoresList } = (() => {
@@ -14,16 +15,23 @@ const { wordList, scoresList } = (() => {
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/words', (req, res) => {
+app.get('/words', (req, res, next) => {
 	const words = randWords(wordList);
+	if (words.length === 0) next({ status: 500, msg: 'Internal Server Error' });
 	res.json({ words });
 });
 
-app.post('/rank', (req, res) => {
-	const archivedScore = req.body.score;
-	const scoresBlow = scoresList.filter((score) => score < archivedScore);
+app.post('/rank', (req, res, next) => {
+	const achivedScore = +req.body.score;
+	if (!achivedScore) next({ status: 400, msg: 'enter valid score' });
+	const scoresBlow = scoresList.filter((score) => score < achivedScore);
 	res.json({ rank: Math.floor((scoresBlow.length / scoresList.length) * 100) });
+});
+
+app.use((err, req, res, next) => {
+	res.status(err.status).json({ error: err.msg });
 });
 
 app.listen(5000, () => {
