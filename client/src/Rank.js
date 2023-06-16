01914f9dd5a1b36763e1e4b9400ac1099ff19365
Elshaabany <elshaabany@protonmail.com>
2023-06-16 18:24:08 +0300

    Fetch the rank form the server

--- client/src/Rank.js
+++ client/src/Rank.js
@@ -1,8 +1,31 @@
-function Rank() {
+import { useEffect, useState } from "react";
+
+function Rank({ score }) {
+  const [rank, setRank] = useState("");
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("http://localhost:5000/rank", {
+          method: "POST",
+          body: JSON.stringify({ score: (+score / 10) * 100 }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        setRank(data.rank);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [score]);
+
+  if (!rank) return <h2>loading...</h2>;
+
   return (
     <>
       <h2>Completed!</h2>
-      <p>your Rank:</p>
+      <p>your Rank: {rank}</p>
       <button>Try Again</button>
     </>
   );
